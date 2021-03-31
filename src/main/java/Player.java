1fc06a1c98ce02397ab613b279fa7046473f29a2
wWEvanWw <31501102@student.publicboard.ca>
2021-03-30 20:27:09 -0400

    Added a lot of stuff
    Player class Trade class,
    gonna do trading now

---
+++ src/main/java/Player.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class Player{
+    private final int DESERT = 0,
+            GRAIN = 1,
+            LUMBER = 2,
+            WOOL = 3,
+            ORE = 4,
+            BRICK = 5;
+    public int[] resources = {0,0,0,0,0,0};
+    public Color color;
+    public int turn;
+    public int victory = 0;
+
+
+    public Player(int tt, Color cc){
+        turn = tt;
+        color = cc;
+    }
+
+    public boolean win(){
+        return victory >= 10;
+    }
+
+    public int resourceNum(int r){
+        return resources[r];
+    }
+
+    public void acceptTrade(int[] trade){
+        for (int i = 1; i < 6; i ++){
+            resources[i] += trade[i];
+        }
+    }
+}
